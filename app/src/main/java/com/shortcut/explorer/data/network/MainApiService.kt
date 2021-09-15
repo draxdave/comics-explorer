3049404b305089cfd965ebed86a6d22079c21ee1
Siamak Mahmoudi <ssiasoft@gmail.com>
2021-09-16 00:42:13 +0430

    Update: Retrofit providers are set up

---
+++ app/src/main/java/com/shortcut/explorer/data/network/MainApiService.kt
@@ -0,0 +1,6 @@
+package com.shortcut.explorer.data.network
+
+
+interface MainApiService {
+
+}
--- app/src/main/java/com/shortcut/explorer/di/module/data/NetworkModule.kt
+++ app/src/main/java/com/shortcut/explorer/di/module/data/NetworkModule.kt
@@ -1,8 +1,49 @@
 package com.shortcut.explorer.di.module.data
 
+import com.shortcut.explorer.BuildConfig
+import com.shortcut.explorer.data.network.MainApiService
 import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
 
 @Module
 class NetworkModule {
 
+    @Provides
+    @Singleton
+    fun provideLoggingInterceptor() = HttpLoggingInterceptor().apply { level=
+        HttpLoggingInterceptor.Level.BODY }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(loggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder().apply {
+
+            if (BuildConfig.DEBUG)
+                addInterceptor(loggingInterceptor)
+
+        }.build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .client(okHttpClient)
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideMainApiService(retrofit: Retrofit): MainApiService {
+        return retrofit.create()
+    }
 }
