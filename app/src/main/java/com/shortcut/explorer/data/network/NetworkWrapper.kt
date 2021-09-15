79fff876509b6b8b6c504629b14d9507b18e7b7a
Siamak Mahmoudi <ssiasoft@gmail.com>
2021-09-16 01:38:48 +0430

    Update: Network Wrapper is done finally.

--- app/src/main/java/com/shortcut/explorer/data/network/NetworkWrapper.kt
+++ app/src/main/java/com/shortcut/explorer/data/network/NetworkWrapper.kt
@@ -19,7 +19,47 @@ import java.net.UnknownHostException
  */
 class NetworkWrapper{
 
+    /**
+     * Triggers the network transaction. [call] lambda contains the network call and will return
+     * network result.
+     *
+     * @param T : request type.
+     * @param call : inline function, containing the network executable.
+     */
+    private suspend fun <T> run(call: suspend () -> Response<ServerResponse<T>>) = call.invoke()
 
+    /**
+     * Jus start the transaction with provided [call] object.
+     *
+     * @param T :Expecting result type
+     * @param call : inline function, containing the network executable.
+     * @return : returns a network result wraped with [Resource]. Inner data is a [ServerResponse].
+     */
+    suspend fun <T> fetch (call:suspend () -> Response<ServerResponse<T>>) = fetch(call,{})
+
+    /**
+     * Jus start the transaction with provided [call] object and invoke [onSuccess] lambda if
+     * the call goes successfully.
+     *
+     * @param T :Expecting result type
+     * @param call : inline function, containing the network executable.
+     * @param onSuccess : inline function, to be invoked if the call returns successfully.
+     * @return : returns a network result wraped with [Resource]. Inner data is a [ServerResponse].
+     */
+    suspend fun <T> fetch (call:suspend () -> Response<ServerResponse<T>>,
+                           onSuccess: OnSuccess<T>
+    ) = fetch(call,onSuccess,{_,_->})
+
+    /**
+     * Jus start the transaction with provided [call] object. Invokes [onSuccess] lambda if
+     * the call goes successfully and [onFail] otherwise.
+     *
+     * @param T :Expecting result type
+     * @param call : inline function, containing the network executable.
+     * @param onSuccess : inline function, to be invoked if the call returns successfully.
+     * @param onFail : inline function, to be invoked if the call fails.
+     * @return : returns a network result wraped with [Resource]. Inner data is a [ServerResponse].
+     */
     suspend fun <T> fetch (call: suspend () -> Response<ServerResponse<T>>,
                            onSuccess: OnSuccess<T>,
                            onFail: OnFail
@@ -62,11 +102,28 @@ class NetworkWrapper{
         }
     }
 
+    /**
+     * Manage anything that should be done hen the result is successful.
+     *
+     * @param T : data type.
+     * @param body : main content that received.
+     * @param onSuccess : passed callback function to invoke.
+     * @return : a [Resource] of type [Status.SUCCESS].
+     */
     private suspend fun <T> onSucceed(body: Response<ServerResponse<T>>, onSuccess: OnSuccess<T>): Resource<ServerResponse<T>>{
         onSuccess(body.body())
         return Resource.success(body.body())
     }
 
+    /**
+     * Extracts error message out of the error body. Since, we have no clear vision of the possible
+     * error bodies, here we pass a sample string "msg" as extracted content.
+     *
+     * @param T : data type.
+     * @param code : error code.
+     * @param onFail : passed callback function to be invoked on transaction failure.
+     * @return : a [Resource] of type [Status.ERROR].
+     */
     private suspend fun <T> onFailed(code: Int, onFail: OnFail): Resource<ServerResponse<T>>{
         return try {
             onFail.invoke("msg",code)
