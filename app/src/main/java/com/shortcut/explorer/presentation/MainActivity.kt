3edb5c7637761ac2d06b9929052c82d005bc4aeb
Siamak Mahmoudi <ssiasoft@gmail.com>
2021-09-16 15:25:05 +0430

    Update: Bottom Nav binded to NavGraph

--- app/src/main/java/com/shortcut/explorer/presentation/MainActivity.kt
+++ app/src/main/java/com/shortcut/explorer/presentation/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.shortcut.explorer.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.shortcut.explorer.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val bottomNavView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        NavigationUI.setupWithNavController(
+            bottomNavView,
+            findNavController(R.id.fragment_container)
+        )
     }
 }
