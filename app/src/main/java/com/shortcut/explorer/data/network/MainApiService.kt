320fd0c16a22d406e441d7be6942125400b90b81
Siamak Mahmoudi <ssiasoft@gmail.com>
2021-09-16 12:08:40 +0430

    Update: Comic item object added
    + Api services are added.

--- app/src/main/java/com/shortcut/explorer/data/network/MainApiService.kt
+++ app/src/main/java/com/shortcut/explorer/data/network/MainApiService.kt
@@ -3,11 +3,15 @@ package com.shortcut.explorer.data.network
 import com.shortcut.explorer.data.network.model.ComicItem
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 
 interface MainApiService {
 
     @GET("info.0.json")
     suspend fun getLatestComic(): Response<ComicItem>
+
+    @GET("{num}/info.0.json")
+    suspend fun getComicByNumber(@Path("num") num:String): Response<ComicItem>
 
 }
---
+++ app/src/main/java/com/shortcut/explorer/data/network/model/ComicItem.kt
@@ -0,0 +1,14 @@
+package com.shortcut.explorer.data.network.model
+
+data class ComicItem(
+    val num: Int,
+    val title: String,
+    val alt: String,
+    val safe_title: String,
+    val transcript: String,
+    val img: String,
+    val year: String,
+    val month: String,
+    val day: String,
+    val news: String,
+    )
