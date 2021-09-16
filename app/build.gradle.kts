0fea672b345a82a160d961d0bb13e580ae95b9bc
Siamak Mahmoudi <ssiasoft@gmail.com>
2021-09-16 12:02:10 +0430

    Update: Server address for main WebApi added to Gradle file.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,7 +70,7 @@ android {
         versionCode = 1
         minSdkVersion(23)
         targetSdkVersion(30)
-        buildConfigField("String", "MAIN_BASE_URL", "")
+        buildConfigField("String", "MAIN_BASE_URL", "\"https://xkcd.com/\"")
         buildConfigField("String", "SEARCH_BASE_URL", "")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- app/src/main/java/com/shortcut/explorer/data/network/MainApiService.kt
+++ app/src/main/java/com/shortcut/explorer/data/network/MainApiService.kt
@@ -1,6 +1,13 @@
 package com.shortcut.explorer.data.network
 
+import com.shortcut.explorer.data.network.model.ComicItem
+import retrofit2.Response
+import retrofit2.http.GET
+
 
 interface MainApiService {
+
+    @GET("info.0.json")
+    suspend fun getLatestComic(): Response<ComicItem>
 
 }
